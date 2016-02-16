805d26d6284cc81d3a5e30cea960f1ff307058d8
Carolina No <carolina.s.no@gmail.com>
2016-02-16 14:37:48 -0500

    info windows are almost there

--- public/js/controllers/marketController.js
+++ public/js/controllers/marketController.js
@@ -4,6 +4,8 @@ map.controller('marketController', ['$scope', 'farmersMarketApi', function($scop
 
   $scope.markets = [];
 
+  $scope.markers = [];
+
   $scope.getAll = function(){
     farmersMarketApi.getAll().then(function(response){
       $scope.markets = $scope.markets.push(response.data);
@@ -17,18 +19,60 @@ map.controller('marketController', ['$scope', 'farmersMarketApi', function($scop
     });
   }
 
-  var farmersMarketMarker;
-
   $scope.marketLatLng = function(){
     for (var i = 0; i < $scope.markets.length; i++) {
-      var marketName = $scope.markets[i].market_name;
+      var marketInfo =
+      '<div class="market-info">' +
+      '<h3 id="market-name">' +
+      $scope.markets[i].market_name +
+      '</h3>' +
+      '<h4 id="market-address">' +
+      $scope.markets[i].address_line_1 + ', ' + $scope.markets[i].city + ', NY' +
+      '</h4>' +
+      '<p id="market-hours">' +
+      'HOURS' +
+      '</br>' +
+      $scope.markets[i].operation_hours +
+      '</p>' +
+      '<p id="market-season">' +
+      'SEASON' +
+      '</br>' +
+      $scope.markets[i].operation_season +
+      '</p>' +
+      '<p id="market-phone">' +
+      'CONTACT' +
+      '</br>' +
+      $scope.markets[i].phone +
+      '</p>' +
+      '</div>';
+
       var marketLocation = {
         lat: $scope.markets[i].latitude,
         lng: $scope.markets[i].longitude
-      }
-      console.log(marketName, marketLocation);
+      };
+
+      console.log(marketLocation);
+
+      var farmersMarketMarkerLL = new google.maps.LatLng(marketLocation.lat, marketLocation.lng);
+
+      var myInfoWindow = new google.maps.InfoWindow({
+        content: marketInfo
+      });
+
+      var marker = new google.maps.Marker({
+        position: farmersMarketMarkerLL,
+        map: myMap.getMap(),
+        animation: google.maps.Animation.DROP,
+        infowindow: myInfoWindow
+      });
+
+      $scope.markers.push(marker);
+
+      marker.addListener('click', function(){
+        console.log('hey girl');
+        myInfoWindow.open(myMap.getMap(), marker);
+      });
     }
-    farmersMarketMarker = new google.maps.LatLng(marketLocation[0], marketLocation[1]);
   };
 
   var myMap = {};
@@ -45,11 +89,15 @@ map.controller('marketController', ['$scope', 'farmersMarketApi', function($scop
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
 
-    this.marker = new google.maps.Marker({
-      position: this.currentLatLng,
-      map: this.map,
-      animation: google.maps.Animation.DROP
-    });
+    // this.marker = new google.maps.Marker({
+    //   position: this.currentLatLng,
+    //   map: this.map,
+    //   animation: google.maps.Animation.DROP
+    // });
+  }
+
+  myMap.getMap = function(){
+    return this.map;
   }
 
   myMap.init();
